c3d14d913d5b49cfd6c4bcb7d3a53b23901e7bdc
AaronEKoch <aaronkoch99@yahoo.com>
2018-07-28 19:46:54 -0500

    socket.io hello world

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var path = require('path');
-var express = require('express');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-var app = express();
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+	io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+	  console.log('user disconnected');
+  });
+});
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
